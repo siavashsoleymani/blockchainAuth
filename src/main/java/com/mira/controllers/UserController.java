a9460492e4020dcbe35f03dfbb541c99787096b6
siavash.solimaniii <siavash.solimanii@yahoo.com>
2019-05-17 11:08:31 +0430

    add the ability to connect to gnache test server

--- src/main/java/com/mira/controllers/UserController.java
+++ src/main/java/com/mira/controllers/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController()
-public class UserController {
+public class    UserController {
     @Autowired
     private UsersServiceImpl userSvc;
 
--- src/main/java/com/mira/services/impl/UsersServiceImpl.java
+++ src/main/java/com/mira/services/impl/UsersServiceImpl.java
@@ -12,8 +12,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.WalletUtils;
+import org.web3j.protocol.admin.Admin;
+import org.web3j.protocol.admin.methods.response.NewAccountIdentifier;
+import org.web3j.protocol.admin.methods.response.PersonalUnlockAccount;
+import org.web3j.protocol.http.HttpService;
 import org.web3j.tuples.generated.Tuple3;
+import org.web3j.tx.*;
+import org.web3j.tx.gas.DefaultGasProvider;
+import org.web3j.tx.response.NoOpProcessor;
 
+import java.io.File;
 import java.math.BigInteger;
 
 @Service
@@ -26,14 +36,23 @@ public class UsersServiceImpl extends ContractService implements InitializingBea
     @Value("${contractaddress}")
     private String contractAddress;
 
-    private final static BigInteger GAS_LIMIT = BigInteger.valueOf(6721975L);
+    private final static BigInteger GAS_LIMIT = BigInteger.valueOf(6721975);
 
-    private final static BigInteger GAS_PRICE = BigInteger.valueOf(20000000000L);
+    private final static BigInteger GAS_PRICE = BigInteger.valueOf(0);
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        usersContract = Users.load(contractAddress,
-                        web3j, credentialSvc.getCredentials(),GAS_PRICE, GAS_LIMIT);
+//        ClientTransactionManager transactionManager = new ClientTransactionManager(web3j, "0x9fa05352f21a02ff9ef2666300c99e1bcffe38c1");
+//        usersContract = Users.load(contractAddress, web3j, transactionManager, GAS_PRICE, GAS_LIMIT);
+//        System.out.println("is contract valid: " + usersContract.isValid());
+//        Credentials credentials = WalletUtils.loadCredentials("siavash", WalletUtils.getDefaultKeyDirectory());
+//        System.out.println(credentials.getAddress());
+
+        String walletFilePath = "/home/siavash/Desktop/myDataDir/keystore/UTC--2019-04-21T08-11-09.057643861Z--9fa05352f21a02ff9ef2666300c99e1bcffe38c1";
+        Credentials credentials = WalletUtils.loadCredentials("siavash", walletFilePath);
+        usersContract = Users.deploy(web3j, credentials, GAS_PRICE, GAS_LIMIT).send();
+        contractAddress = usersContract.getContractAddress();
+        System.out.println(contractAddress + usersContract.isValid());
     }
 
     public User addUser(User user, MultipartFile file) throws Exception {
@@ -45,13 +64,13 @@ public class UsersServiceImpl extends ContractService implements InitializingBea
         return user;
     }
 
-    public User probeUser(MultipartFile image) throws Exception{
+    public User probeUser(MultipartFile image) throws Exception {
         FingerprintTemplate probe = getTemplateFromImage(image);
 
         return getMatchedUserUsingMatcher(probe);
     }
 
-    private FingerprintTemplate getTemplateFromImage(MultipartFile image) throws Exception{
+    private FingerprintTemplate getTemplateFromImage(MultipartFile image) throws Exception {
         byte[] probeImage = image.getBytes();
         FingerprintTemplate probe = new FingerprintTemplate()
                 .dpi(400)
@@ -59,14 +78,14 @@ public class UsersServiceImpl extends ContractService implements InitializingBea
         return probe;
     }
 
-    private User getMatchedUserUsingMatcher(FingerprintTemplate probe) throws Exception{
+    private User getMatchedUserUsingMatcher(FingerprintTemplate probe) throws Exception {
         FingerprintMatcher matcher = new FingerprintMatcher().index(probe);
         User user = null;
         String match = null;
         double high = 0;
 
         int size = usersContract.getTemplatesSize().send().intValue();
-        for (long i=0; i < size; i++) {
+        for (long i = 0; i < size; i++) {
             String candidate = usersContract.userTemplates(BigInteger.valueOf(i)).send();
 
             double score = matcher.match(new FingerprintTemplate().deserialize(candidate));
@@ -84,7 +103,7 @@ public class UsersServiceImpl extends ContractService implements InitializingBea
         return user;
     }
 
-    private void checkIfUserExists(FingerprintTemplate candidate) throws Exception{
+    private void checkIfUserExists(FingerprintTemplate candidate) throws Exception {
         User user = getMatchedUserUsingMatcher(candidate);
         if (user != null) throw new UserAlreadyExistException(env.getProperty("userexists"));
 
